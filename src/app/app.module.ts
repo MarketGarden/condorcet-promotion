e04283b3246c56fdd6d1025b0d14cb660b034b13
Gardent Marc <marcgardent@yahoo.fr>
2017-04-08 11:05:55 +0200

    chore:add simulation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SimulationComponent } from './simulation/simulation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimulationComponent
   ],
   imports: [
     BrowserModule,
